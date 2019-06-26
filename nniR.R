74ed7fd04fb6c6fb21a1e714b33e996d33609fc1
AlexKnyshov <alexknyshov@gmail.com>
2019-06-25 17:49:47 -0700

    help added

--- nniR.R
+++ nniR.R
@@ -1,6 +1,11 @@
 library (ape)
 library (phangorn)
 args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 0){
+  cat("Syntax: Rscript nniR.R [path to the tree file]\n")
+  cat("Example: Rscript nniR.R tree.tre \n")
+  quit()
+}
 start <- read.tree(args[1])
 nnis <- nni(start)
 write.tree(c(start, nnis), "pls_nni_tree.tre")
