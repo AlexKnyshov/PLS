6b7a4ddc3b3eec5884cbe5aeb573e242ff080705
AlexKnyshov <alexknyshov@gmail.com>
2019-06-30 14:25:54 -0700

    manual range specification added

--- PLS.py
+++ PLS.py
@@ -5,10 +5,29 @@ if len(sys.argv) >= 3:
 	tab = sys.argv[2]
 	if opt == "-prt":
 		prts = sys.argv[3]
+	elif opt == "-sl":
+		if sys.argv[3] and sys.argv[4]:
+			start = int(sys.argv[3])
+			end = int(sys.argv[4])
+		else:
+			start = 0
+			end = -1
+	elif opt[:3] == "-st":
+		if sys.argv[3] and sys.argv[4]:
+			start = int(sys.argv[3])
+			end = int(sys.argv[4])
+		else:
+			start = 0
+			end = -1
+	else:
+		print "incorrect command, exiting"
+		sys.exit()
 else:
-	print "FORMAT: python PLS.py [option: -prt (use partition file), -sl (use per site calculation), -stN (sum every N positions)] [path to .sitelh file] [path to partition file]"
+	print "FORMAT: python PLS.py [option: -prt (use partition file), -sl (use per site calculation), -stN (sum every N positions)] [path to .sitelh file] [path to partition file] ([start] [end])"
 	print "EXAMPLE: python PLS.py -prt analysis.sitelh partitions.txt"
 	print "EXAMPLE: python PLS.py -st10 analysis.sitelh"
+	print "EXAMPLE: python PLS.py -st10 analysis.sitelh 1 2075"
+	print "EXAMPLE: python PLS.py -sl analysis.sitelh 2075 3028"
 	sys.exit()
 
 tabout = {}
@@ -31,16 +50,30 @@ if opt == "-prt":
 			row_ranges = row_eq[1].strip().split(",")
 			if len(row_ranges) == 1:
 				prtout[row_prt] = [int(i) for i in row_ranges[0].strip().split("-")]
+				print "partition", row_prt, "ranges", prtout[row_prt][0], prtout[row_prt][-1]
 			else:
 				for r in range(len(row_ranges)):
 					prtout[row_prt+"_"+str(r+1)] = [int(i) for i in row_ranges[r].strip().split("-")]
+					print "partition", row_prt+"_"+str(r+1), "ranges", prtout[row_prt+"_"+str(r+1)][0], prtout[row_prt+"_"+str(r+1)][-1]
 elif opt == "-sl":
-	for i in range(1,len(tabout[1])):
-		prtout[i] = [i,i+1]
+	if start == 0 and end == -1:
+		print "site likelihoods, beginning to end"
+		for i in range(1,len(tabout[1])):
+			prtout[i] = [i,i+1]
+	else:
+		print "site likelihoods, start", start, "end", end
+		for i in range(start,end+1):
+			prtout[i] = [i,i+1]
 elif opt[:3] == "-st":
 	step = int(opt[3:])
-	for i in range(1,len(tabout[1]),step):
-		prtout[i] = [i,i+step]
+	if start == 0 and end == -1:
+		print "site likelihoods, beginning to end, step", step
+		for i in range(1,len(tabout[1]),step):
+			prtout[i] = [i,i+step]
+	else:
+		print "site likelihoods, start", start, "end", end, "step", step
+		for i in range(start,end+1,step):
+			prtout[i] = [i,i+step]
 
 finaout = open("pls_prtlls.csv","w")
 print >> finaout, "partition,"+",".join([str(i) for i in sorted(tabout.keys())])
